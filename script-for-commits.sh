ee048c864e8fc8273a82facb0c076df0dbca152a
Utkarsh Maurya <projects.utkarshMaurya@gmail.com>
2024-06-26 14:30:58 +0530

    Update script-for-commits.sh

--- script-for-commits.sh
+++ script-for-commits.sh
@@ -3,21 +3,22 @@
 
 total_commits=$(git rev-list --count HEAD)
 
-dest="PATH"
+dest="./DEST"
 
 # Repository URL
-repo="REPO_LINK"
+repo="REPO_URL"
+    cd "$dest"
 
 
 for ((commit=1; commit<=$total_commits; commit++)); do
     echo "Checking out commit number: $commit"
-    
-    mkdir -p "$dest/$commit"
-    git clone -q $repo "$dest/$commit"
-    cd "$dest/$commit"
-    commit_hash=$(git rev-list --reverse HEAD | sed -n ${commit}p)
-    
-    git checkout -q $commit_hash
 
+    mkdir -p "$commit"
+    git clone -q $repo "$commit"
+    cd "$commit"
+    commit_hash=$(git rev-list --reverse HEAD | sed -n ${commit}p)
+    git checkout -q $commit_hash
     git reset --hard -q $commit_hash
+    
+    cd ..
 done
